8aace29264518f637b6988e7d3c8dfd121f3d78a
Oriya124578 <oriya.margalit@post.runi.ac.il>
2025-11-06 23:04:44 +0200

    finish 4

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,30 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+
+		int N = Integer.parseInt(args[0]);
+		double sum = 0.0;
+		double Sign = 1.0;
+		double denominator = 1;
+		for (int i = 0; i < N; i++) {
+			sum = sum + Sign / denominator;
+			Sign = -Sign;
+			denominator = denominator + 2;
+		}
+
+		double pi = 4 * sum;
+
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:     " + pi);
+
+
+
+		
+
+		
+
+
+
+
 	}
 }
